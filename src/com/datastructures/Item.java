a549d162fc442f7b42ecd0d6e94fab064d570423
Vamsi Kankipati <vamsibh1@gmail.com>
2020-04-23 16:15:58 +0530

    List implementation in progress

---
+++ src/com/datastructures/Item.java
@@ -0,0 +1,15 @@
+package com.datastructures;
+
+public class Item {
+
+	String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+}
---
+++ src/com/datastructures/List.java
@@ -0,0 +1,30 @@
+package com.datastructures;
+
+public class List<T> {
+
+	public class Node {
+		Item item;
+		Node next;
+
+		public Item getItem() {
+			return item;
+		}
+
+		public void setItem(Item item) {
+			this.item = item;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
+
+	private void addNode(Object value) {
+		// Implementation
+	}
+
+}
