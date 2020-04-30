06f7a6f517a43ceb9bcbc3021dbce04d25d5c717
Vamsi Kankipati <vamsibh1@gmail.com>
2020-04-30 19:16:49 +0530

    Insertion sort - increasing and decreasing orders

--- src/com/algorithms/InsertionSort.java
+++ src/com/algorithms/InsertionSort.java
@@ -3,13 +3,10 @@ package com.algorithms;
 public class InsertionSort {
 
 	/*
-	 * Static method that returns sorted array using insertion sort method
-	 *
-	 * @param int[]
-	 * 
-	 * @return int[]
+	 * Static method that returns sorted array using insertion sort method in
+	 * increasing order
 	 */
-	public static int[] insertionSort(int[] arr) {
+	public static int[] increasing(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
 			int key = arr[i];
 			int j = i - 1;
@@ -23,13 +20,37 @@ public class InsertionSort {
 	}
 
 	/*
+	 * Static method that returns sorted array using insertion sort method in
+	 * decreasing order
+	 */
+	public static int[] decreasing(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+			while ((j >= 0) && (arr[j] < key)) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+				arr[j + 1] = key;
+			}
+		}
+		return arr;
+	}
+
+	/*
 	 * Main method to test an example integer array
 	 */
 	public static void main(String[] args) {
-		int[] ar = { 5, 2, 6, 4, 7, 3, 8 };
-		insertionSort(ar);
+		int[] ar = { 31, 41, 59, 26, 41, 58 };
+		System.out.println("Increasing: ");
+		increasing(ar);
 		for (int a : ar) {
 			System.out.print(a + " ");
 		}
+		System.out.println("\nDecreasing: ");
+		decreasing(ar);
+		for (int a : ar) {
+			System.out.print(a + " ");
+		}
+
 	}
 }
